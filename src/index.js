392a5aecb49882dd5370df98d00208ccf20cd056
Andrey <andrey.liubashchenko@gmail.com>
2023-04-15 01:09:23 +0300

    LocalStorage

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import './index.css';
 import App from './App';
 
-import { store } from './store';
+import { store, persistor } from './store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-    	<App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
